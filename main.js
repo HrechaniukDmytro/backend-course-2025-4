919aa818f3bf229bf8854fd3dec2bba90df48910
Гречанюк Дмитро bc2025-4 <dmytro.hrechaniuk@lnu.edu.ua>
2025-10-19 21:20:45 +0300

    Add main program

---
+++ main.js
@@ -0,0 +1,73 @@
+const { Command } = require('commander');
+const http = require('http');
+const fs = require('fs').promises;
+const { XMLBuilder } = require('fast-xml-parser');
+
+const program = new Command();
+program
+  .requiredOption('-i, --input <path>', 'input JSON file')
+  .requiredOption('-h, --host <host>', 'server host')
+  .requiredOption('-p, --port <port>', 'server port');
+
+program.parse(process.argv);
+const options = program.opts();
+
+async function readJsonFile(path)
+{
+  // Перевірка існування файлу
+  try {
+    const files = await fs.readdir('.');
+    if (!files.includes(path))
+    {
+      console.error('Cannot find input file:', path);
+      process.exit(1);
+    }
+    
+    // Читання та парсинг
+    const data = await fs.readFile(path, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    // Обробка інших помилок читання/парсингу
+    console.error('Cannot find input file', error.message);
+    process.exit(1);
+  }
+}
+
+const server = http.createServer(async (req, res) => 
+{
+  const url = new URL(req.url, `http://${options.host}:${options.port}`);
+  const params = url.searchParams;
+
+  const data = await readJsonFile(options.input);
+
+  const minPetalLength = parseFloat(params.get('min_petal_length'));
+  const includeVariety = params.get('variety') === 'true';
+
+  let filtered = data;
+  if (!isNaN(minPetalLength)) 
+  {
+    filtered = filtered.filter(item => item["petal.length"] > minPetalLength);
+  }
+
+  const xmlData = filtered.map(item => 
+  {
+    const flower = 
+    {
+      petal_length: item["petal.length"],
+      petal_width: item["petal.width"]
+    };
+    if (includeVariety) flower.variety = item.variety;
+    return flower;
+  });
+
+  const builder = new XMLBuilder({ ignoreAttributes: false, format: true });
+  const xmlContent = builder.build({ irises: { flower: xmlData } });
+
+  res.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' });
+  res.end(xmlContent);
+});
+
+server.listen(options.port, options.host, () => 
+{
+  console.log(`Server running at http://${options.host}:${options.port}/`);
+});
